8c472ec79f9c9c8224e9798ef220a225cceeabb5
KISOONG JANG <kisjang@kr.ibm.com>
2023-06-29 00:18:11 +0900

    initial commit

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
 app.post("/webhook", (req, res) => {
-  console.log(JSON.stringify(req.body));
+  console.log(req.body);
   res.send(req.body);
 });
 
