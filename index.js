810972c049f7f728ec5209a18926ef5c460894f6
KISOONG JANG <kisjang@kr.ibm.com>
2023-06-29 00:07:48 +0900

    initial commit

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+app.post("/webhook", (req, res) => {
+  res.send(req.body);
+});
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
